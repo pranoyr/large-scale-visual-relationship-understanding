5c11ac3550d1d4d41ba87b2c9819af189e371536
Pranoy R <pranoyr@Pranoy-2.local>
2020-10-04 18:27:25 +0530

    initial commit

--- config.py
+++ config.py
@@ -16,7 +16,6 @@ import torch.nn as nn
 from torch.nn import init
 import yaml
 
-import nn as mynn
 from utils.collections import AttrDict
 
 __C = AttrDict()
@@ -1055,174 +1054,174 @@ __C.DEBUG = False
 # [Infered value]
 __C.PYTORCH_VERSION_LESS_THAN_040 = False
 
-# ---------------------------------------------------------------------------- #
-# mask heads or keypoint heads that share res5 stage weights and
-# training forward computation with box head.
-# ---------------------------------------------------------------------------- #
-_SHARE_RES5_HEADS = set(
-    [
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v0upshare',
-    ]
-)
+# # ---------------------------------------------------------------------------- #
+# # mask heads or keypoint heads that share res5 stage weights and
+# # training forward computation with box head.
+# # ---------------------------------------------------------------------------- #
+# _SHARE_RES5_HEADS = set(
+#     [
+#         'mask_rcnn_heads.mask_rcnn_fcn_head_v0upshare',
+#     ]
+# )
 
 
-def assert_and_infer_cfg(make_immutable=True):
-    """Call this function in your script after you have finished setting all cfg
-    values that are necessary (e.g., merging a config from a file, merging
-    command line config options, etc.). By default, this function will also
-    mark the global cfg as immutable to prevent changing the global cfg settings
-    during script execution (which can lead to hard to debug errors or code
-    that's harder to understand than is necessary).
-    """
-    if __C.MODEL.RPN_ONLY or __C.MODEL.FASTER_RCNN:
-        __C.RPN.RPN_ON = True
-    if __C.RPN.RPN_ON or __C.RETINANET.RETINANET_ON:
-        __C.TEST.PRECOMPUTED_PROPOSALS = False
-    if __C.MODEL.LOAD_IMAGENET_PRETRAINED_WEIGHTS:
-        assert __C.RESNETS.IMAGENET_PRETRAINED_WEIGHTS or __C.VGG16.IMAGENET_PRETRAINED_WEIGHTS, \
-            "Path to the weight file must not be empty to load imagenet pertrained resnets."
-    if set([__C.MRCNN.ROI_MASK_HEAD, __C.KRCNN.ROI_KEYPOINTS_HEAD]) & _SHARE_RES5_HEADS:
-        __C.MODEL.SHARE_RES5 = True
-    if version.parse(torch.__version__) < version.parse('0.4.0'):
-        __C.PYTORCH_VERSION_LESS_THAN_040 = True
-        # create alias for PyTorch version less than 0.4.0
-        init.uniform_ = init.uniform
-        init.normal_ = init.normal
-        init.constant_ = init.constant
-        nn.GroupNorm = mynn.GroupNorm
-    if make_immutable:
-        cfg.immutable(True)
+# def assert_and_infer_cfg(make_immutable=True):
+#     """Call this function in your script after you have finished setting all cfg
+#     values that are necessary (e.g., merging a config from a file, merging
+#     command line config options, etc.). By default, this function will also
+#     mark the global cfg as immutable to prevent changing the global cfg settings
+#     during script execution (which can lead to hard to debug errors or code
+#     that's harder to understand than is necessary).
+#     """
+#     if __C.MODEL.RPN_ONLY or __C.MODEL.FASTER_RCNN:
+#         __C.RPN.RPN_ON = True
+#     if __C.RPN.RPN_ON or __C.RETINANET.RETINANET_ON:
+#         __C.TEST.PRECOMPUTED_PROPOSALS = False
+#     if __C.MODEL.LOAD_IMAGENET_PRETRAINED_WEIGHTS:
+#         assert __C.RESNETS.IMAGENET_PRETRAINED_WEIGHTS or __C.VGG16.IMAGENET_PRETRAINED_WEIGHTS, \
+#             "Path to the weight file must not be empty to load imagenet pertrained resnets."
+#     if set([__C.MRCNN.ROI_MASK_HEAD, __C.KRCNN.ROI_KEYPOINTS_HEAD]) & _SHARE_RES5_HEADS:
+#         __C.MODEL.SHARE_RES5 = True
+#     if version.parse(torch.__version__) < version.parse('0.4.0'):
+#         __C.PYTORCH_VERSION_LESS_THAN_040 = True
+#         # create alias for PyTorch version less than 0.4.0
+#         init.uniform_ = init.uniform
+#         init.normal_ = init.normal
+#         init.constant_ = init.constant
+#         nn.GroupNorm = mynn.GroupNorm
+#     if make_immutable:
+#         cfg.immutable(True)
 
 
-def merge_cfg_from_file(cfg_filename):
-    """Load a yaml config file and merge it into the global config."""
-    with open(cfg_filename, 'r') as f:
-        yaml_cfg = AttrDict(yaml.load(f))
-    _merge_a_into_b(yaml_cfg, __C)
+# def merge_cfg_from_file(cfg_filename):
+#     """Load a yaml config file and merge it into the global config."""
+#     with open(cfg_filename, 'r') as f:
+#         yaml_cfg = AttrDict(yaml.load(f))
+#     _merge_a_into_b(yaml_cfg, __C)
 
-cfg_from_file = merge_cfg_from_file
-
-
-def merge_cfg_from_cfg(cfg_other):
-    """Merge `cfg_other` into the global config."""
-    _merge_a_into_b(cfg_other, __C)
+# cfg_from_file = merge_cfg_from_file
 
 
-def merge_cfg_from_list(cfg_list):
-    """Merge config keys, values in a list (e.g., from command line) into the
-    global config. For example, `cfg_list = ['TEST.NMS', 0.5]`.
-    """
-    assert len(cfg_list) % 2 == 0
-    for full_key, v in zip(cfg_list[0::2], cfg_list[1::2]):
-        # if _key_is_deprecated(full_key):
-        #     continue
-        # if _key_is_renamed(full_key):
-        #     _raise_key_rename_error(full_key)
-        key_list = full_key.split('.')
-        d = __C
-        for subkey in key_list[:-1]:
-            assert subkey in d, 'Non-existent key: {}'.format(full_key)
-            d = d[subkey]
-        subkey = key_list[-1]
-        assert subkey in d, 'Non-existent key: {}'.format(full_key)
-        value = _decode_cfg_value(v)
-        value = _check_and_coerce_cfg_value_type(
-            value, d[subkey], subkey, full_key
-        )
-        d[subkey] = value
-
-cfg_from_list = merge_cfg_from_list
+# def merge_cfg_from_cfg(cfg_other):
+#     """Merge `cfg_other` into the global config."""
+#     _merge_a_into_b(cfg_other, __C)
 
 
-def _merge_a_into_b(a, b, stack=None):
-    """Merge config dictionary a into config dictionary b, clobbering the
-    options in b whenever they are also specified in a.
-    """
-    assert isinstance(a, AttrDict), 'Argument `a` must be an AttrDict'
-    assert isinstance(b, AttrDict), 'Argument `b` must be an AttrDict'
+# def merge_cfg_from_list(cfg_list):
+#     """Merge config keys, values in a list (e.g., from command line) into the
+#     global config. For example, `cfg_list = ['TEST.NMS', 0.5]`.
+#     """
+#     assert len(cfg_list) % 2 == 0
+#     for full_key, v in zip(cfg_list[0::2], cfg_list[1::2]):
+#         # if _key_is_deprecated(full_key):
+#         #     continue
+#         # if _key_is_renamed(full_key):
+#         #     _raise_key_rename_error(full_key)
+#         key_list = full_key.split('.')
+#         d = __C
+#         for subkey in key_list[:-1]:
+#             assert subkey in d, 'Non-existent key: {}'.format(full_key)
+#             d = d[subkey]
+#         subkey = key_list[-1]
+#         assert subkey in d, 'Non-existent key: {}'.format(full_key)
+#         value = _decode_cfg_value(v)
+#         value = _check_and_coerce_cfg_value_type(
+#             value, d[subkey], subkey, full_key
+#         )
+#         d[subkey] = value
 
-    for k, v_ in a.items():
-        full_key = '.'.join(stack) + '.' + k if stack is not None else k
-        # a must specify keys that are in b
-        if k not in b:
-            # if _key_is_deprecated(full_key):
-            #     continue
-            # elif _key_is_renamed(full_key):
-            #     _raise_key_rename_error(full_key)
-            # else:
-            raise KeyError('Non-existent config key: {}'.format(full_key))
-
-        v = copy.deepcopy(v_)
-        v = _decode_cfg_value(v)
-        v = _check_and_coerce_cfg_value_type(v, b[k], k, full_key)
-
-        # Recursively merge dicts
-        if isinstance(v, AttrDict):
-            try:
-                stack_push = [k] if stack is None else stack + [k]
-                _merge_a_into_b(v, b[k], stack=stack_push)
-            except BaseException:
-                raise
-        else:
-            b[k] = v
+# cfg_from_list = merge_cfg_from_list
 
 
-def _decode_cfg_value(v):
-    """Decodes a raw config value (e.g., from a yaml config files or command
-    line argument) into a Python object.
-    """
-    # Configs parsed from raw yaml will contain dictionary keys that need to be
-    # converted to AttrDict objects
-    if isinstance(v, dict):
-        return AttrDict(v)
-    # All remaining processing is only applied to strings
-    if not isinstance(v, six.string_types):
-        return v
-    # Try to interpret `v` as a:
-    #   string, number, tuple, list, dict, boolean, or None
-    try:
-        v = literal_eval(v)
-    # The following two excepts allow v to pass through when it represents a
-    # string.
-    #
-    # Longer explanation:
-    # The type of v is always a string (before calling literal_eval), but
-    # sometimes it *represents* a string and other times a data structure, like
-    # a list. In the case that v represents a string, what we got back from the
-    # yaml parser is 'foo' *without quotes* (so, not '"foo"'). literal_eval is
-    # ok with '"foo"', but will raise a ValueError if given 'foo'. In other
-    # cases, like paths (v = 'foo/bar' and not v = '"foo/bar"'), literal_eval
-    # will raise a SyntaxError.
-    except ValueError:
-        pass
-    except SyntaxError:
-        pass
-    return v
+# def _merge_a_into_b(a, b, stack=None):
+#     """Merge config dictionary a into config dictionary b, clobbering the
+#     options in b whenever they are also specified in a.
+#     """
+#     assert isinstance(a, AttrDict), 'Argument `a` must be an AttrDict'
+#     assert isinstance(b, AttrDict), 'Argument `b` must be an AttrDict'
+
+#     for k, v_ in a.items():
+#         full_key = '.'.join(stack) + '.' + k if stack is not None else k
+#         # a must specify keys that are in b
+#         if k not in b:
+#             # if _key_is_deprecated(full_key):
+#             #     continue
+#             # elif _key_is_renamed(full_key):
+#             #     _raise_key_rename_error(full_key)
+#             # else:
+#             raise KeyError('Non-existent config key: {}'.format(full_key))
+
+#         v = copy.deepcopy(v_)
+#         v = _decode_cfg_value(v)
+#         v = _check_and_coerce_cfg_value_type(v, b[k], k, full_key)
+
+#         # Recursively merge dicts
+#         if isinstance(v, AttrDict):
+#             try:
+#                 stack_push = [k] if stack is None else stack + [k]
+#                 _merge_a_into_b(v, b[k], stack=stack_push)
+#             except BaseException:
+#                 raise
+#         else:
+#             b[k] = v
 
 
-def _check_and_coerce_cfg_value_type(value_a, value_b, key, full_key):
-    """Checks that `value_a`, which is intended to replace `value_b` is of the
-    right type. The type is correct if it matches exactly or is one of a few
-    cases in which the type can be easily coerced.
-    """
-    # The types must match (with some exceptions)
-    type_b = type(value_b)
-    type_a = type(value_a)
-    if type_a is type_b:
-        return value_a
+# def _decode_cfg_value(v):
+#     """Decodes a raw config value (e.g., from a yaml config files or command
+#     line argument) into a Python object.
+#     """
+#     # Configs parsed from raw yaml will contain dictionary keys that need to be
+#     # converted to AttrDict objects
+#     if isinstance(v, dict):
+#         return AttrDict(v)
+#     # All remaining processing is only applied to strings
+#     if not isinstance(v, six.string_types):
+#         return v
+#     # Try to interpret `v` as a:
+#     #   string, number, tuple, list, dict, boolean, or None
+#     try:
+#         v = literal_eval(v)
+#     # The following two excepts allow v to pass through when it represents a
+#     # string.
+#     #
+#     # Longer explanation:
+#     # The type of v is always a string (before calling literal_eval), but
+#     # sometimes it *represents* a string and other times a data structure, like
+#     # a list. In the case that v represents a string, what we got back from the
+#     # yaml parser is 'foo' *without quotes* (so, not '"foo"'). literal_eval is
+#     # ok with '"foo"', but will raise a ValueError if given 'foo'. In other
+#     # cases, like paths (v = 'foo/bar' and not v = '"foo/bar"'), literal_eval
+#     # will raise a SyntaxError.
+#     except ValueError:
+#         pass
+#     except SyntaxError:
+#         pass
+#     return v
 
-    # Exceptions: numpy arrays, strings, tuple<->list
-    if isinstance(value_b, np.ndarray):
-        value_a = np.array(value_a, dtype=value_b.dtype)
-    elif isinstance(value_b, six.string_types):
-        value_a = str(value_a)
-    elif isinstance(value_a, tuple) and isinstance(value_b, list):
-        value_a = list(value_a)
-    elif isinstance(value_a, list) and isinstance(value_b, tuple):
-        value_a = tuple(value_a)
-    else:
-        raise ValueError(
-            'Type mismatch ({} vs. {}) with values ({} vs. {}) for config '
-            'key: {}'.format(type_b, type_a, value_b, value_a, full_key)
-        )
-    return value_a
+
+# def _check_and_coerce_cfg_value_type(value_a, value_b, key, full_key):
+#     """Checks that `value_a`, which is intended to replace `value_b` is of the
+#     right type. The type is correct if it matches exactly or is one of a few
+#     cases in which the type can be easily coerced.
+#     """
+#     # The types must match (with some exceptions)
+#     type_b = type(value_b)
+#     type_a = type(value_a)
+#     if type_a is type_b:
+#         return value_a
+
+#     # Exceptions: numpy arrays, strings, tuple<->list
+#     if isinstance(value_b, np.ndarray):
+#         value_a = np.array(value_a, dtype=value_b.dtype)
+#     elif isinstance(value_b, six.string_types):
+#         value_a = str(value_a)
+#     elif isinstance(value_a, tuple) and isinstance(value_b, list):
+#         value_a = list(value_a)
+#     elif isinstance(value_a, list) and isinstance(value_b, tuple):
+#         value_a = tuple(value_a)
+#     else:
+#         raise ValueError(
+#             'Type mismatch ({} vs. {}) with values ({} vs. {}) for config '
+#             'key: {}'.format(type_b, type_a, value_b, value_a, full_key)
+#         )
+#     return value_a
