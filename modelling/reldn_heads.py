288051fa3c5baf1613379a7fbb4517656f6277ff
Pranoy R <pranoyr@Pranoy-2.local>
2020-10-07 22:34:36 +0530

    cleared

--- modelling/reldn_heads.py
+++ modelling/reldn_heads.py
@@ -33,17 +33,10 @@ class reldn_head(nn.Module):
 			nn.Linear(1024 * 3, 1024),
 			nn.LeakyReLU(0.1),
 			nn.Linear(1024, 1024))
-		# if not cfg.MODEL.USE_SEM_CONCAT:
-		#     self.prd_sem_embeddings = nn.Sequential(
-		#         nn.Linear(300, 1024),
-		#         nn.LeakyReLU(0.1),
-		#         nn.Linear(1024, 1024))
-		# else:
 		self.prd_sem_hidden = nn.Sequential(
 			nn.Linear(300, 1024),
 			nn.LeakyReLU(0.1),
 			nn.Linear(1024, 1024))
-		# self.prd_sem_embeddings = nn.Linear(3 * 1024, 1024)
 		self.prd_sem_embeddings = nn.Sequential(
 				nn.Linear(300, 1024),
 				nn.LeakyReLU(0.1),
@@ -55,23 +48,8 @@ class reldn_head(nn.Module):
 			nn.LeakyReLU(0.1),
 			nn.Linear(1024, 1024))
 			
-	# spo_feat is concatenation of SPO
-	# def forward(self, spo_feat=None, sbj_labels=None, obj_labels=None, sbj_feat=None, obj_feat=None):
 	def forward(self, spo_feat=None, sbj_feat=None, obj_feat=None):
-		
-		# sbj_labels = torch.cat(sbj_labels, dim=0)
-		# obj_labels = torch.cat(obj_labels, dim=0)
-
-		# # device_id = spo_feat.get_device()
 		device = sbj_feat.device
-		# if sbj_labels is not None:
-		#     sbj_labels = Variable(torch.from_numpy(sbj_labels.astype('int64'))).to(device_id)
-		# if obj_labels is not None:
-		#     obj_labels = Variable(torch.from_numpy(obj_labels.astype('int64'))).to(device_id)
-			
-		# if spo_feat.dim() == 4:
-		#     spo_feat = spo_feat.squeeze(3).squeeze(2)
-		
 		sbj_vis_embeddings = self.so_vis_embeddings(sbj_feat)
 		obj_vis_embeddings = self.so_vis_embeddings(obj_feat)
 		
@@ -92,9 +70,7 @@ class reldn_head(nn.Module):
 		obj_vis_embeddings = F.normalize(obj_vis_embeddings, p=2, dim=1)  # (#bs, 1024)
 		obj_sim_matrix = torch.mm(obj_vis_embeddings, so_sem_embeddings)  # (#bs, #prd)
 		obj_cls_scores = 3 * obj_sim_matrix
-		
-		
-		# if not cfg.MODEL.USE_SEM_CONCAT:
+
 		ds_prd_vecs = self.prd_vecs
 		ds_prd_vecs = Variable(torch.from_numpy(ds_prd_vecs.astype('float32'))).to(device)
 		prd_sem_embeddings = self.prd_sem_embeddings(ds_prd_vecs)
@@ -102,12 +78,11 @@ class reldn_head(nn.Module):
 		prd_vis_embeddings = F.normalize(prd_vis_embeddings, p=2, dim=1)  # (#bs, 1024)
 		prd_sim_matrix = torch.mm(prd_vis_embeddings, prd_sem_embeddings.t_())  # (#bs, #prd)
 		prd_cls_scores = 3 * prd_sim_matrix
-		# else:
+		
 
 		if not self.training:
 			sbj_cls_scores = F.softmax(sbj_cls_scores, dim=1)
 			obj_cls_scores = F.softmax(obj_cls_scores, dim=1)
 			prd_cls_scores = F.softmax(prd_cls_scores, dim=1)
-		
-		#return prd_cls_scores, sbj_cls_scores, obj_cls_scores
+
 		return sbj_cls_scores, obj_cls_scores, prd_cls_scores
