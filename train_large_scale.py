e66ab5a3cfad2dcfb76d1d47a48ddf0fc263d4a6
Pranoy R <pranoyr@Pranoy-2.local>
2020-10-08 23:19:15 +0530

    torch.save

--- train_large_scale.py
+++ train_large_scale.py
@@ -36,6 +36,40 @@ from modelling.model import FasterRCNN
 from opts import parse_opts
 
 
+def train_epoch(model, dataloader, optimizer, epoch):
+    model.train()
+    for i, data in enumerate(dataloader):
+        images, targets = data
+        _, losses = model(images, targets)
+        final_loss = losses["loss_objectness"] + losses["loss_rpn_box_reg"] + \
+            losses["loss_classifier"] + losses["loss_box_reg"] + \
+            losses["loss_sbj"] + losses["loss_obj"] + losses["loss_rlp"]
+
+        optimizer.zero_grad()
+        final_loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 10 == 0:
+            print(f"""RCNN_Loss    : {final_loss.item()}
+					rpn_cls_loss   : {losses['loss_objectness'].item()}
+					rpn_reg_loss   : {losses['loss_rpn_box_reg'].item()}
+					box_loss 	   : {losses['loss_box_reg']}
+					cls_loss       : {losses['loss_classifier']}
+					sbj_loss	   : {losses['loss_sbj']}
+					obj_loss	   : {losses['loss_obj']}
+					sbj_acc        : {losses['acc_sbj']}
+					obj_acc	       : {losses['acc_obj']}
+					rlp_loss   	   : {losses['loss_rlp']}				 
+					rlp_acc 	   : {losses['acc_rlp']}\n"""
+                  )
+
+    state = {'epoch': epoch, 'state_dict': model.state_dict(
+    ), 'optimizer_state_dict': optimizer.state_dict()}
+    torch.save(state, os.path.join(
+        'snapshots', f'large_scale_vrd-Epoch-{epoch}.pth'))
+    print(f"Epoch {epoch} model saved!\n")
+
+
 def resume_model(opt, model, optimizer):
     """ Resume model 
     """
@@ -63,38 +97,8 @@ def main_worker():
     if opt.weight_path:
         resume_model(opt, faster_rcnn, optimizer)
 
-    faster_rcnn.train()
     for epoch in range(1, cfg.N_EPOCHS+1):
-        for i, data in enumerate(dataloader):
-            images, targets = data
-            _, losses = faster_rcnn(images, targets)
-            final_loss = losses["loss_objectness"] + losses["loss_rpn_box_reg"] + \
-                losses["loss_classifier"] + losses["loss_box_reg"] + \
-                losses["loss_sbj"] + losses["loss_obj"] + losses["loss_rlp"]
-
-            optimizer.zero_grad()
-            final_loss.backward()
-            optimizer.step()
-
-            if (i + 1) % 10 == 0:
-                print(f"""RCNN_Loss    : {final_loss.item()}
-						rpn_cls_loss   : {losses['loss_objectness'].item()}
-						rpn_reg_loss   : {losses['loss_rpn_box_reg'].item()}
-						box_loss 	   : {losses['loss_box_reg']}
-						cls_loss       : {losses['loss_classifier']}
-						sbj_loss	   : {losses['loss_sbj']}
-						obj_loss	   : {losses['loss_obj']}
-						sbj_acc        : {losses['acc_sbj']}
-						obj_acc	       : {losses['acc_obj']}
-						rlp_loss   	   : {losses['loss_rlp']}				 
-						rlp_acc 	   : {losses['acc_rlp']}\n"""
-                      )
-
-        state = {'epoch': epoch, 'state_dict': faster_rcnn.state_dict(
-        ), 'optimizer_state_dict': optimizer.state_dict()}
-        torch.save(state, os.path.join(
-            'snapshots', f'large_scale_vrd-Epoch-{epoch}.pth'))
-        print(f"Epoch {epoch} model saved!\n")
+        train_epoch(faster_rcnn, dataloader, optimizer, epoch)
 
 
 if __name__ == "__main__":
