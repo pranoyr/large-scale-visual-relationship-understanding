445749b315dc7341b10339da5c01b798c8510a46
Pranoy R <pranoyr@Pranoy-2.local>
2020-10-08 18:26:40 +0530

    updated

--- opts.py
+++ opts.py
@@ -3,7 +3,7 @@ import argparse
 
 def parse_opts():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weight_file', type=str,
+    parser.add_argument('--weight_path', type=str,
                         help='path to weight file')
     args = parser.parse_args()
 
