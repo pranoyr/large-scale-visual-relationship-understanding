8c91449ebb3ad1f6dcb6524bada8906188102627
Pranoy R <pranoyr@Pranoy-2.local>
2020-12-13 01:10:28 +0530

    initial commi

--- train_large_scale.py
+++ train_large_scale.py
@@ -216,7 +216,7 @@ def main_worker():
 	if opt.scheduler == "plateau":
 		scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5)
 	elif opt.scheduler == "multi_step":
-		scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[8, 12])
+		scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[83631, 111508])
 	elif opt.scheduler == "step_lr":
 		scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1, last_epoch=-1)
 
