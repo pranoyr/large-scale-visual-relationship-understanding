ac739054abf2b1930ab163796e106f66b641dada
Pranoy R <pranoyr@Pranoy-2.local>
2021-04-20 20:23:08 +0530

    updated for ibs

--- datasets/custom_vrd.py
+++ datasets/custom_vrd.py
@@ -91,6 +91,8 @@ class VRDDataset(Dataset):
 			gt_sbj_bbox, gt_obj_bbox = ast.literal_eval(spo[1]), ast.literal_eval(spo[4])
 			predicate = spo[2]
 
+			if gt_sbj_label=="person" or gt_obj_label=="person":
+    				continue
 			print(gt_sbj_label, predicate, gt_obj_label )
 
 			# prepare bboxes for subject and object
