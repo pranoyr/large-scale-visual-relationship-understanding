4af8ef12845946bc092e2c349bacda9b31e7be6c
Pranoy R <pranoyr@Pranoy-2.local>
2021-04-21 22:55:56 +0530

    updated rlp dataloader

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ __C = AttrDict()
 #   from fast_rcnn_config import cfg
 cfg = __C
 
-__C.DEVICE = 'cuda:1'
+__C.DEVICE = 'cpu'
 
 #
 # Training options
@@ -175,7 +175,7 @@ __C.RPN.POSITIVE_FRACTION = 0.5
 #
 
 __C.TEST = AttrDict()
-__C.TEST.THRESHOLD = 0.1
+__C.TEST.THRESHOLD = 0.8
 
 
 #
@@ -183,7 +183,7 @@ __C.TEST.THRESHOLD = 0.1
 #
 
 __C.DATASET_DIR = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), 'data', 'Aircraft')
+    os.path.abspath(__file__)), 'data', 'VRD')
 
 __C.WORD_VECTORS_DIR = os.path.join(os.path.dirname(os.path.abspath(
     __file__)), 'data', 'wordvectors', 'GoogleNews-vectors-negative300.bin')
