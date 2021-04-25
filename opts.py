404e090b5a73a8eee7fb5ccd470c7f7c31ddfbf1
Pranoy R <pranoyr@Pranoy-2.local>
2021-04-25 10:44:52 +0530

    updated train_vrd.py

--- opts.py
+++ opts.py
@@ -9,7 +9,7 @@ def parse_opts():
 						help='input image path')
 	parser.add_argument('--batch_size', type=int, default=2,
 						help='batch size')
-	parser.add_argument('--lr', type=int,
+	parser.add_argument('--lr', type=float,
 						help='learning rate')
 	parser.add_argument('--num_workers', type=int, default=0,
 						help='number of workers for data loaders')
