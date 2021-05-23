e2fb6b43ac52097b5072c23d6f8b6b40ba9aeab6
Pranoy R <pranoyr@Pranoy-2.local>
2021-05-23 14:04:27 +0530

    vg dataset

--- dataset.py
+++ dataset.py
@@ -1,10 +1,13 @@
 from datasets.vrd import VRDDataset
 from datasets.custom_vrd import CustomDataset
+from datasets.vg import VGDataset
 
 
 def get_training_data(cfg):
     if cfg.DATASET == 'VRD':
         training_data = VRDDataset(cfg.DATASET_DIR, 'train')
+    elif cfg.DATASET == 'VG':
+        training_data = VGDataset(cfg.DATASET_DIR, 'train')
     else:
         training_data = CustomDataset(cfg.DATASET_DIR, 'train')
 
@@ -14,6 +17,8 @@ def get_training_data(cfg):
 def get_validation_data(cfg):
     if cfg.DATASET == 'VRD':
         validation_data = VRDDataset(cfg.DATASET_DIR, 'test')
+    elif cfg.DATASET == 'VG':
+        validation_data = VGDataset(cfg.DATASET_DIR, 'test')
     else:
         validation_data = CustomDataset(cfg.DATASET_DIR, 'test')
 
