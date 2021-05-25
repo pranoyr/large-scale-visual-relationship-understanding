cf7e615fd3d26321d11929c3b67d6e44f14830da
Pranoy R <pranoyr@Pranoy-2.local>
2021-05-25 20:22:49 +0530

    updated mean.py

--- mean.py
+++ mean.py
@@ -1,33 +1,70 @@
-class MyDataset(Dataset):
-    def __init__(self):
-        self.data = torch.randn(100, 3, 24, 24)
-        
-    def __getitem__(self, index):
-        x = self.data[index]
-        return x
+import math
+import os
+import pdb
+import random
+import time
+from collections import OrderedDict
+import utils.net as net_utils
 
-    def __len__(self):
-        return len(self.data)
-    
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.models.detection._utils as det_utils
+import torchvision.utils as vutils
+from torch.autograd import Variable, gradcheck
+from torch.autograd.gradcheck import gradgradcheck
+from torch.jit.annotations import Dict, List, Optional, Tuple
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.models.detection.faster_rcnn import (FastRCNNPredictor,
+                                                      GeneralizedRCNNTransform,
+                                                      MultiScaleRoIAlign,
+                                                      TwoMLPHead)
+from torchvision.models.detection.rpn import (AnchorGenerator,
+                                              RegionProposalNetwork, RPNHead)
+from torchvision.models.resnet import resnet101
+from torchvision.ops import boxes as box_ops
+from dataset import get_training_data, get_validation_data
 
-dataset = MyDataset()
-loader = DataLoader(
-    dataset,
-    batch_size=10,
-    num_workers=1,
-    shuffle=False
-)
+from config import cfg
+from datasets.vrd import collater
+from modelling.model import FasterRCNN
+from opts import parse_opts
+from utils.util import AverageMeter, Metrics, ProgressMeter
 
 
 mean = 0.
 std = 0.
 nb_samples = 0.
-for data in loader:
-    batch_samples = data.size(0)
-    data = data.view(batch_samples, data.size(1), -1)
-    mean += data.mean(2).sum(0)
-    std += data.std(2).sum(0)
-    nb_samples += batch_samples
+
+seed = 1
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+
+opt = parse_opts()
+train_data = get_training_data(cfg)
+val_data = get_validation_data(cfg)
+train_loader = DataLoader(
+    train_data, num_workers=opt.num_workers, collate_fn=collater, batch_size=opt.batch_size, shuffle=True)
+
+for data in train_loader:
+    images, targets = data
+    images = images.view(images.size(0), images.size(1), -1)
+    mean += images.mean(2).sum(0)
+    std += images.std(2).sum(0)
+    nb_samples += images.size(0)
 
 mean /= nb_samples
 std /= nb_samples
+
+print(mean)
+print(std)
+
+    
