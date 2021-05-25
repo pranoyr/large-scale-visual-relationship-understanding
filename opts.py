869df24935087ed42b69c7351b3aab73d23322a7
Pranoy R <pranoyr@Pranoy-2.local>
2021-05-25 15:26:57 +0530

    updated opts

--- opts.py
+++ opts.py
@@ -7,11 +7,11 @@ def parse_opts():
 						help='path to weight file')
 	parser.add_argument('--image_path', type=str,
 						help='input image path')
-	parser.add_argument('--batch_size', type=int, default=2,
+	parser.add_argument('--batch_size', type=int, default=4,
 						help='batch size')
 	parser.add_argument('--lr', type=float,
 						help='learning rate')
-	parser.add_argument('--num_workers', type=int, default=4,
+	parser.add_argument('--num_workers', type=int, default=0,
 						help='number of workers for data loaders')
 	parser.add_argument('--max_iter', type=int, default=125446,
 						help='number of iterations')
