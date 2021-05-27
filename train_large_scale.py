ab4e503e776ec70160cb6e63f7be4f9f9934db38
Pranoy R <pranoyr@Pranoy-2.local>
2021-05-27 17:28:02 +0530

    removed multigpu

--- train_large_scale.py
+++ train_large_scale.py
@@ -116,9 +116,9 @@ def main_worker():
 
     faster_rcnn = FasterRCNN()
 
-    if torch.cuda.device_count() > 1 and opt.multi_gpu :
-        print("Let's use", torch.cuda.device_count(), "GPUs!")
-        faster_rcnn = nn.DataParallel(faster_rcnn)
+    # if torch.cuda.device_count() > 1 and opt.multi_gpu :
+    #     print("Let's use", torch.cuda.device_count(), "GPUs!")
+    #     faster_rcnn = nn.DataParallel(faster_rcnn)
 
     # loading model from a ckpt
     if opt.weight_path:
