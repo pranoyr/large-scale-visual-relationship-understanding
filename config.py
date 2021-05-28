44f652353f364d2d207d5c9c4af61203d7465c67
Pranoy R <pranoyr@Pranoy-2.local>
2021-05-28 15:08:15 +0530

    updated with sh

--- config.py
+++ config.py
@@ -40,22 +40,6 @@ __C.TRAIN.BACKBONE_LR_SCALAR = 0.1
 # E.g., 'step', 'steps_with_decay', ...
 __C.TRAIN.LR_POLICY = 'step'
 
-# Some LR Policies (by example):
-# 'step'
-#   lr = TRAIN.BASE_LR * TRAIN.GAMMA ** (cur_iter // TRAIN.STEP_SIZE)
-# 'steps_with_decay'
-#   TRAIN.STEPS = [0, 60000, 80000]
-#   TRAIN.GAMMA = 0.1
-#   lr = TRAIN.BASE_LR * TRAIN.GAMMA ** current_step
-#   iters [0, 59999] are in current_step = 0, iters [60000, 79999] are in
-#   current_step = 1, and so on
-# 'steps_with_lrs'
-#   TRAIN.STEPS = [0, 60000, 80000]
-#   TRAIN.LRS = [0.02, 0.002, 0.0002]
-#   lr = LRS[current_step]
-
-# Hyperparameter used by the specified policy
-# For 'step', the current LR is multiplied by TRAIN.GAMMA at each step
 __C.TRAIN.GAMMA = 0.1
 
 # Uniform step size for 'steps' policy
